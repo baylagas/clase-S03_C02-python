c5bdddf46f791abc8f5b446b765673d3d24af2a2
balbino aylagas <balbino_a@hotmail.com>
2020-06-12 18:36:20 -0600

    init files

---
+++ 01-stringOperators.py
@@ -0,0 +1,10 @@
+# string operators
+# + concatenacion
+# * copia n el texto
+
+mensaje1 = "hola a todos"
+mensaje2 = ", bien bien por aca con suenio"
+
+print(mensaje1 + mensaje2)
+print(mensaje1 + str(2))
+print("*" * 100)
